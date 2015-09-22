eee7930081c2960f16924a3ff40299caca5b3691
ruzicho <ruzicho1234@gmail.com>
2015-09-22 18:57:59 -0400

    assignment 4
    unobtrusive javascript

---
+++ somewhere.js
@@ -0,0 +1,12 @@
+var x = document.getElementsByClassName('popup');
+
+for (var i=0;i<x.length;i++=1)
+{
+
+	x[i].onclick = function ()
+	{
+
+		window.open(this.href,'popup','arguments');
+		return false;
+	}
+}
