3bba744abc2493d02845091062c230248483ac73
ruzicho <ruzicho1234@gmail.com>
2015-12-02 16:42:44 -0500

    update

---
+++ IT-2320/Week 13 - Introduction to AngularJS/Assignment12/IntroductionToAngular/Scripts/Home.js
@@ -0,0 +1,16 @@
+﻿function GetBattingOrder($scope)
+{
+    $scope.viewOrder.list = [
+
+   { FirstName: "Kenny", LastName: "Lofton" },
+   { FirstName: "Omar", LastName: "Vizquel" },
+   { FirstName: "Carlos", LastName: "Baerga" },
+   { FirstName: "Albert", LastName: "Belle" },
+   { FirstName: "Eddie", LastName: "Murray" },
+   { FirstName: "Jim", LastName: "Thome" },
+   { FirstName: "Manny", LastName: "Ramirez" },
+   { FirstName: "Paul", LastName: "Sorrento" },
+   { FirstName: "Sandy", LastName: "Alomar" }
+
+    ];
+}
